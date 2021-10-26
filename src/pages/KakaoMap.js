48a6f867e7666683b0ae129238f9d4eba28cec86
sooyoungL <soo.lee@viewmagine.com>
2021-10-26 10:34:42 +0900

    FoodRoad first commit

---
+++ src/pages/KakaoMap.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import "./kakaomap.css";
+import Map from "../Component/Map/Map";
+
+function KakaoMap() {
+  return (
+    <div className="kakaomap">
+      <Map />
+    </div>
+  );
+}
+
+export default KakaoMap;
